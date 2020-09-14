3e7f7690234b47a7ea75a4c1dfeb4db314a5c38a
basedantoni <anthony.mercado300@gmail.com>
2020-09-14 08:33:39 -0500

    refactor

--- server.js
+++ server.js
@@ -10,9 +10,9 @@ const app = express();
 
 const spotify = require('./routes/api/spotify');
 
-const readImage = require('./readImage');
+// const readImage = require('./readImage');
 
-readImage();
+// readImage();
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // update to match the domain you will make the request from
