b7e829ea384efd22825c185773444f3a374a4943
basedantoni <anthony.mercado300@gmail.com>
2020-09-16 13:24:10 -0500

    successfully fetched types for each pokemon

--- client/src/components/Pokemon.js
+++ client/src/components/Pokemon.js
@@ -1,9 +1,10 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 
-const Pokemon = ({ name, onClick }) => {
+const Pokemon = ({ name, pokeIndex, onClick }) => {
   return (
     <div>
-      <button onClick={() => onClick()}>{name}</button>
+      <button onClick={() => onClick()}><img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokeIndex + 1}.png`} alt={`pokemon-no-${pokeIndex + 1}`} /></button>
     </div>
   )
 }
--- client/src/components/PokemonList.js
+++ client/src/components/PokemonList.js
@@ -8,23 +8,29 @@ const PokemonList = () => {
   useEffect(() => {
     async function fetchPokemon() {
       const res = await axios.get('https://pokeapi.co/api/v2/pokemon?limit=151')
-
-      setPokemon(res.data.results)
+      let pokemonArr = res.data.results
+      // Get types for each Pokemon
+      pokemonArr.map(async poke => {
+        let res = await axios.get(poke.url)
+        poke.types = res.data.types
+      })
+      setPokemon(pokemonArr)
     }
     fetchPokemon()
-  })
+  }, [])
 
   // Team State
   const [team, setTeam] = useState([])
   const [teamCount, setTeamCount] = useState(0)
 
-  const addPokemon = (name) => {
+  const addPokemon = (name, pokeIndex) => {
     if(teamCount < 6) {
       // Increments Team Count
       setTeamCount(teamCount + 1)
-
+      // Create Pokemon object
+      const pokemon = { name, pokeIndex }
       // Pushes New Team Member to Team
-      team.push(name)
+      team.push(pokemon)
       setTeam(team)
     } else {
       console.log('Party is full')
@@ -34,6 +40,8 @@ const PokemonList = () => {
   const removePokemon = (index) => {
     if(teamCount > 0) {
       setTeamCount(teamCount - 1)
+
+      // Filters team based on the index of the array selected
       setTeam(team.filter((_, i) => i !== index))
     } else {
       console.log('Party is empty')
@@ -44,10 +52,11 @@ const PokemonList = () => {
     <div>
       <h2>Choose Your Team</h2>
       {pokemon.map(({ name, url }, index) => (
-        <Pokemon onClick={() => addPokemon(name)} url={url} name={name} key={index} />
+        <Pokemon onClick={() => addPokemon(name, index)} pokeIndex={index} name={name} key={url} url={url} />
       ))}
-      {team.map((name, index) => (
-        <Pokemon onClick={() => removePokemon(index)} name={name} key={index} />
+      <h1>Team</h1>
+      {team.map(({ name, pokeIndex }, index) => (
+        <Pokemon onClick={() => removePokemon(index)} pokeIndex={pokeIndex} name={name} key={index} />
       ))}
     </div>
   )
