f027e18da2701d9f40bdd4a79d3ad5c36cad8bf5
basedantoni <anthony.mercado300@gmail.com>
2020-09-17 20:00:29 -0500

    uninstalled and reinstalled CRA

--- client/build/precache-manifest.7d1b4c454666aaf88310504bc1700085.js
+++ client/build/precache-manifest.7d1b4c454666aaf88310504bc1700085.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "24b32081758fb64e2e870bddc493b47c",
+    "revision": "41ee4b9bb5c662c3e694c583ab9f3b4b",
     "url": "/index.html"
   },
   {
-    "revision": "abc37d7f107005fefc2a",
-    "url": "/static/js/2.f125f54e.chunk.js"
+    "revision": "670f795a484650ddeb5c",
+    "url": "/static/js/2.0f866d59.chunk.js"
   },
   {
     "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
-    "url": "/static/js/2.f125f54e.chunk.js.LICENSE.txt"
+    "url": "/static/js/2.0f866d59.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "fff3e34e30a330f206e6",
-    "url": "/static/js/main.08bb10b6.chunk.js"
+    "revision": "99db17282a26feddb019",
+    "url": "/static/js/main.3fdc93b6.chunk.js"
   },
   {
     "revision": "9b4e396c183e42c1fa5c",
--- client/build/service-worker.js
+++ client/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.7e97fbec8ba143128e8731df50b5df25.js"
+  "/precache-manifest.7d1b4c454666aaf88310504bc1700085.js"
 );
 
 self.addEventListener('message', (event) => {
