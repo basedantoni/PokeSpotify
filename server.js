e3f01d56a54a1fa7aeff07b25faec30e7013ca37
basedantoni <anthony.mercado300@gmail.com>
2020-09-23 12:33:50 -0500

    heroku config

--- server.js
+++ server.js
@@ -10,9 +10,12 @@ const app = express();
 
 const spotify = require('./routes/api/spotify');
 
-// const readImage = require('./readImage');
-
-// readImage();
+if (process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'staging') {
+  app.use(express.static('client/build'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname + '/client/build/index.html'));
+  });
+}
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // update to match the domain you will make the request from
