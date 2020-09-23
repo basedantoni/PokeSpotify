cc5643c5be57ca7205bc865295390ffd52f6ccd6
basedantoni <anthony.mercado300@gmail.com>
2020-09-23 12:56:09 -0500

    deploying

--- server.js
+++ server.js
@@ -10,13 +10,6 @@ const app = express();
 
 const spotify = require('./routes/api/spotify');
 
-if (process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'staging') {
-  app.use(express.static('client/build'));
-  app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname + '/client/build/index.html'));
-  });
-}
-
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // update to match the domain you will make the request from
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
@@ -46,5 +39,12 @@ app.get('/', (req, res) => res.redirect('http://localhost:3000'));
 
 // Use routes
 app.use('/api/spotify', spotify);
+
+if (process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'staging') {
+  app.use(express.static('client/build'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname + 'client', 'build', 'index.html'));
+  });
+}
 
 app.listen(port, () => console.log('Serving running on port ' + port));
